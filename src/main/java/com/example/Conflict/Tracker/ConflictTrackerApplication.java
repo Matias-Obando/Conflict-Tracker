f846dffccf398000a023e045071a386bf050671a
Mattmax9 <matt14012012@gmail.com>
2025-11-18 16:09:13 +0100

    primer commit

---
+++ src/main/java/com/example/Conflict/Tracker/ConflictTrackerApplication.java
@@ -0,0 +1,15 @@
+package com.example.Conflict.Tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConflictTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConflictTrackerApplication.class, args);
+	}
+
+
+
+}
