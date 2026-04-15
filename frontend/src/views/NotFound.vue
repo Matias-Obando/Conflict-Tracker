9e586fbf92da045590451eed72a3187253689d70
Matias-Obando <matiasobando@elpuig.xeill.net>
2026-04-15 10:32:07 +0200

    Delete frontend directory

--- frontend/src/views/NotFound.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div style="padding:40px;text-align:center">
-    <h2>Página no encontrada</h2>
-    <p>La ruta solicitada no existe.</p>
-    <router-link to="/">Volver al inicio</router-link>
-  </div>
-</template>
